971579d9c264dee57990ee55b632c0a63fa068db
Gustav Hjertman <gustav.hjertman@evry.com>
2018-01-26 10:43:32 +0100

    First commit

---
+++ src/main/java/AnalyzeText.java
@@ -0,0 +1,126 @@
+import edu.stanford.nlp.coref.CorefCoreAnnotations;
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.coref.data.Mention;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.parser.nndep.DependencyParser;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.time.TimeAnnotations;
+import edu.stanford.nlp.time.TimeExpression;
+import edu.stanford.nlp.trees.GrammaticalStructure;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+public class AnalyzeText {
+    private StanfordCoreNLP pipeline;
+    private String text;
+    private Properties props;
+    private DependencyParser dp;
+
+    public AnalyzeText (StanfordCoreNLP pipeline, String text, DependencyParser dp){
+        this.pipeline = pipeline;
+        this.text = text;
+        this.dp = dp;
+    }
+
+    public void analyze() {
+
+        System.out.println("---");
+        Annotation document = new Annotation(text);
+        pipeline.annotate(document);
+
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+        List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
+
+        for (CoreMap sentence : sentences) {
+
+            //Print out the sentiment of each sentence
+            //printSentiment(sentence);
+
+            // traversing the words in the current sentence
+            // a CoreLabel is a CoreMap with additional token-specific methods
+            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(CoreAnnotations.TextAnnotation.class);
+                // this is the POS tag of the token
+                String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+                // this is the NER label of the token
+                String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+
+                System.out.println(String.format("Word: [%s], Pos: [%s], Ne: [%s]", word, pos, ne));
+            }
+        }
+
+        System.out.println("---");
+
+        //Print out coref chains and mentions
+        printCorefChains(document, sentences);
+
+        //Print out the dependency tree
+        printDependencyTree(sentences, dp);
+
+        //Print out time
+        printTime(document);
+    }
+
+    private void printSentiment(CoreMap sentence) {
+
+        System.out.println("---");
+        System.out.println("Sentiment:");
+        String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+        System.out.println(sentiment + "\t" + sentence);
+        System.out.println("---");
+    }
+
+    private void printCorefChains(Annotation document, List<CoreMap> sentences) {
+
+        System.out.println("---");
+        System.out.println("coref chains");
+
+        for (CorefChain cc : document.get(CorefCoreAnnotations.CorefChainAnnotation.class).values()) {
+            System.out.println("\t" + cc);
+        }
+        for (CoreMap sentence : document.get(CoreAnnotations.SentencesAnnotation.class)) {
+            System.out.println("---");
+            System.out.println("mentions");
+            for (Mention m : sentence.get(CorefCoreAnnotations.CorefMentionsAnnotation.class)) {
+                System.out.println("\t" + m);
+            }
+        }
+    }
+
+    private void printDependencyTree(List<CoreMap> sentences, DependencyParser dp) {
+
+        for (CoreMap sentence : sentences) {
+            GrammaticalStructure gs = dp.predict(sentence);
+            System.out.println("Dependency tree: " + gs.toString());
+        }
+        System.out.println("---");
+    }
+
+    private void printTime(Annotation document) {
+
+        //Get and format current date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dateFormat.format(new Date());
+        document.set(CoreAnnotations.DocDateAnnotation.class, date);
+
+        pipeline.annotate(document);
+
+        System.out.println("Sentence: " + document.get(CoreAnnotations.TextAnnotation.class));
+        List<CoreMap> timexAnnsAll = document.get(TimeAnnotations.TimexAnnotations.class);
+        System.out.println("Time:");
+
+        for (CoreMap cm : timexAnnsAll) {
+            List<CoreLabel> tokens = cm.get(CoreAnnotations.TokensAnnotation.class);
+            System.out.println(cm.get(TimeExpression.Annotation.class).getTemporal());
+        }
+        System.out.println("---");
+    }
+}
---
+++ src/main/java/CoreNlpExample.java
@@ -0,0 +1,35 @@
+import edu.stanford.nlp.parser.nndep.DependencyParser;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.time.TimeAnnotator;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+public class CoreNlpExample {
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, mention, coref, sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        String modelPath = DependencyParser.DEFAULT_MODEL;
+        DependencyParser dp = DependencyParser.loadFromModelFile(modelPath);
+        Scanner sc = new Scanner(System.in);
+
+        //Add annotators for SUTime
+        pipeline.addAnnotator(new TimeAnnotator("sutime", props));
+
+        //String text = "Kosgi Santosh sent fifty-six letters to Stanford University on the 14th of June 1887, the first at 7:45 AM. All of them were ignored. Now he is very angry!";
+        //String text = "President Xi Jingping of China, on his first state visit to the united States, showed off his familiarity with American history and pop culture on Tuesday night.";
+        //String text = "Three interesting dates are 18 Feb 1997, the 20th of july and 4 days from today. The first date occured on February 18th 21 years ago.";
+        while(true) {
+            System.out.print("Text: ");
+            String text = sc.nextLine();
+            if(text.equalsIgnoreCase("q")){
+                break;
+            }
+            AnalyzeText analyze = new AnalyzeText(pipeline, text, dp);
+            analyze.analyze();
+        }
+    }
+}
